f3278531d0c534858781ba884c555b1d72cc7d36
arun0180 <biradararun333@gmail.com>
2025-11-12 14:18:51 +0530

    almost

--- server/node_modules/better-auth/dist/plugins/admin/access/index.mjs
+++
@@ -1,3 +0,0 @@
-export { b as adminAc, a as defaultAc, c as defaultRoles, d as defaultStatements, u as userAc } from '../../../shared/better-auth.CP-SWMbr.mjs';
-import '../../../shared/better-auth.BLxPPg5G.mjs';
-import '@better-auth/core/error';
--- server/node_modules/better-auth/dist/plugins/organization/access/index.mjs
+++
@@ -1,3 +0,0 @@
-export { b as adminAc, a as defaultAc, c as defaultRoles, d as defaultStatements, m as memberAc, o as ownerAc } from '../../../shared/better-auth.DevHHPLH.mjs';
-import '../../../shared/better-auth.BLxPPg5G.mjs';
-import '@better-auth/core/error';
